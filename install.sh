6d7ec0e84df84dc5d521020c0b77ea4467a39e14
Marcos Chavarría Teijeiro <chavarria1991@gmail.com>
2018-12-09 13:15:44 +0100

    Engadir backup do antigo ficheiro zshrc

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ git clone --recurse-submodules git@github.com:chavaone/config-files.git $CONFIGD
 #Install oh-my-zsh
 ln -s $CONFIGDIR/zsh/ch01_theme.zsh-theme $CONFIGDIR/zsh/oh-my-zsh/themes/
 ln -s $CONFIGDIR/zsh/ch01_theme_root.zsh-theme $CONFIGDIR/zsh/oh-my-zsh/themes/
+mv ~/.zshrc ~/.zshrc.old
 ln -s $CONFIGDIR/zsh/zshrc ~/.zshrc
 
 #Install gitconfig
